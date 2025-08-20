9b6d948d83529127f2f1f6d73de37d244309233d
hakamqadi <hakam.h.qadi@gmail.com>
2025-08-20 22:42:46 +0300

    2.1 - Update Data

--- FootballPlayers.API/Data/DataExtensions.cs
+++ FootballPlayers.API/Data/DataExtensions.cs
@@ -1,14 +1,44 @@
-using System;
+// using System;
+// using Microsoft.EntityFrameworkCore;
+
+// namespace FootballPlayers.API.Data;
+
+// public static class DataExtensions
+// {
+//     public static async Task MigrateDbAsync(this WebApplication app)
+//     {
+//         using var scope = app.Services.CreateScope();
+//         var dbContext = scope.ServiceProvider.GetRequiredService<FootballContext>();
+//         await dbContext.Database.MigrateAsync();
+//     }
+// }
+
+// using FootballPlayers.API.Data;
+// using Microsoft.EntityFrameworkCore;
+
+// namespace FootballPlayers.API;
+
+// public static class DataExtensions
+// {
+//     public static void AddDatabase(this IServiceCollection services, IConfiguration config)
+//     {
+//         services.AddDbContext<FootballContext>(options =>
+//             options.UseSqlite(config.GetConnectionString("DefaultConnection")));
+//     }
+// }
+
+using FootballPlayers.API.Data;
 using Microsoft.EntityFrameworkCore;
 
-namespace FootballPlayers.API.Data;
+namespace FootballPlayers.API;
 
 public static class DataExtensions
 {
-    public static async Task MigrateDbAsync(this WebApplication app)
+    public static void AddDatabase(this IServiceCollection services, IConfiguration config)
     {
-        using var scope = app.Services.CreateScope();
-        var dbContext = scope.ServiceProvider.GetRequiredService<FootballContext>();
-        await dbContext.Database.MigrateAsync();
+        var connectionString = config.GetConnectionString("DefaultConnection");
+
+        services.AddDbContext<FootballContext>(options =>
+            options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
     }
 }
